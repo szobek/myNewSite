69fe87671f89b5a0b3b8cb613453b11c9cb6cf3d
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-03-03 16:02:53 +0100

    Add logo images and implement work showcase section in HTML and CSS

--- index.js
+++ index.js
@@ -11,26 +11,84 @@ skills = [
     { name: 'Java', empty: 3, full: 2 },
     { name: 'Docker', empty: 3, full: 2 },
 ];
+works = [
+    {
+        title: "Létezz könnyedén",
+        description: "Az egész oldalt én csináltam Angularral a megrendelő ötlete szerint készült a design.",
+        image: "./assets/létezz_könnyedén_logo.png",
+        logos: ["assets/angular_logo.png"]
+    },
+    
+    {
+        title: "BME Környezetgazdaság tanszék",
+        description: "A backendet is én csináltam (Laravel ) és a frontendet a bladek jelenítik meg (SSR).",
+        image: "./assets/bme_logo.png",
+        logos: ["assets/laravel_logo.png","assets/mysql_logo.png"]
+    },
+    {
+        title: "Létezz könnyedén",
+        description: "Az egész oldalt én csináltam Angularral a megrendelő ötlete szerint készült a design.",
+        image: "./assets/létezz_könnyedén_logo.png",
+        logos: ["assets/angular_logo.png"]
+    },
+    {
+        title: "Létezz könnyedén",
+        description: "Az egész oldalt én csináltam Angularral a megrendelő ötlete szerint készült a design.",
+        image: "./assets/létezz_könnyedén_logo.png",
+        logos: ["assets/angular_logo.png"]
+    },
+    {
+        title: "Létezz könnyedén",
+        description: "Az egész oldalt én csináltam Angularral a megrendelő ötlete szerint készült a design.",
+        image: "./assets/létezz_könnyedén_logo.png",
+        logos: ["assets/angular_logo.png"]
+    },
+    {
+        title: "Létezz könnyedén",
+        description: "Az egész oldalt én csináltam Angularral a megrendelő ötlete szerint készült a design.",
+        image: "./assets/létezz_könnyedén_logo.png",
+        logos: ["assets/angular_logo.png"]
+    },
+
+]
 document.addEventListener('DOMContentLoaded', function () {
     writeSkillList();
+    writeWorkList();
 });
 
 
 function writeSkillList() {
     const skilldiv = document.getElementById('skills');
     let text = ``;
-    for(let skill of skills){
+    for (let skill of skills) {
         text += `<div class="d-flex align-items-center">`;
-        for(let box of Array.from({length:skill.full})){
+        for (let box of Array.from({ length: skill.full })) {
             text += `<div class="skill-box full"></div>`;
         }
-        for(let box of Array.from({length:skill.empty})){
+        for (let box of Array.from({ length: skill.empty })) {
             text += `<div class="skill-box empty"></div>`;
         }
         text += `<div>${skill.name}</div>`;
         text += `</div>`;
-        
+
     }
     skilldiv.innerHTML = text;
 
+}
+function writeWorkList() {
+    const workdiv = document.getElementById('works');
+    let text = ``;
+    for (let work of works) {
+        text += `<div class="work">`;
+        text += `<h3>${work.title}</h3>`;
+        text += `<p>${work.description}</p>`;
+        text += `<img src="${work.image}" class="site_logo" alt="${work.title}">`;
+        text += `<div class="dev-logo-wrapper">`;
+        for (let logo of work.logos) {
+            text += `<img src="${logo}" class="dev_logo" alt="${work.title}">`;
+        }
+        text += `</div>`;
+        text += `</div>`;
+    }
+    workdiv.innerHTML = text;
 }

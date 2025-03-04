dcb1672ae860b2d02ab0f2dc7cc038b31835a417
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-03-04 14:54:44 +0100

    Add data module for skills and works, refactor index.js to use imported data

---
+++ data.js
@@ -0,0 +1,54 @@
+export const data = {
+    skills:[
+         { name: 'HTML', empty: 0, full: 5 },
+            { name: 'JavaScript', empty: 0, full: 5 },
+            { name: 'Angular', empty: 0, full: 5 },
+            { name: 'TypeScript', empty: 0, full: 5 },
+            { name: 'SCSS', empty: 0, full: 5 },
+            { name: 'SEO', empty: 2, full: 3 },
+            { name: 'PHP', empty: 2, full: 3 },
+            { name: 'MySQL', empty: 2, full: 3 },
+            { name: 'WordPress', empty: 2, full: 3 },
+            { name: 'Java', empty: 2, full: 3 },
+            { name: 'Docker', empty: 3, full: 2 },
+    ],
+    works:[
+        {
+            title: "Létezz könnyedén",
+            description: "Az egész oldalt én csináltam Angularral a megrendelő ötlete szerint készült a design.",
+            image: "./assets/létezz_könnyedén_logo.png",
+            logos: ["assets/angular_logo.png"]
+        },
+        
+        {
+            title: "BME Környezetgazdaság tanszék",
+            description: "A backendet is én csináltam (Laravel ) és a frontendet a bladek jelenítik meg (SSR).",
+            image: "./assets/bme_logo.png",
+            logos: ["assets/laravel_logo.png","assets/mysql_logo.png"]
+        },
+        {
+            title: "Zöldséges",
+            description: "A frontendet csináltam ebben a projektben, együtt működve az UI csapattal és backendes csapattal",
+            image: "./assets/zoldseges_logo.png",
+            logos: ["assets/angular_logo.png"]
+        },
+        {
+            title: "Futárom",
+            description: "A frontendet csináltam ebben a projektben, együtt működve az UI csapattal és backendes csapattal",
+            image: "./assets/futarom_logo.png",
+            logos: ["assets/JS_logo.png"]
+        },
+        {
+            title: "Pimpa",
+            description: "A backendet is én csináltam ( Laravel ) és a frontendet a bladek jelenítik meg (SSR).",
+            image: "./assets/pimpa_logo.png",
+            logos: ["assets/laravel_logo.png","assets/mysql_logo.png"]
+        },
+        {
+            title: "Ammarna",
+            description: "A frontendet ( Angular ) csináltam ebben a projektben, együtt működve az UI csapattal és backendes csapattal",
+            image: "./assets/ammarna_logo.png",
+            logos: ["assets/angular_logo.png"]
+        }
+    ]
+}
--- index.js
+++ index.js
@@ -1,56 +1,5 @@
-skills = [
-    { name: 'HTML', empty: 0, full: 5 },
-    { name: 'JavaScript', empty: 0, full: 5 },
-    { name: 'Angular', empty: 0, full: 5 },
-    { name: 'TypeScript', empty: 0, full: 5 },
-    { name: 'SCSS', empty: 0, full: 5 },
-    { name: 'SEO', empty: 2, full: 3 },
-    { name: 'PHP', empty: 2, full: 3 },
-    { name: 'MySQL', empty: 2, full: 3 },
-    { name: 'WordPress', empty: 2, full: 3 },
-    { name: 'Java', empty: 2, full: 3 },
-    { name: 'Docker', empty: 3, full: 2 },
-];
-works = [
-    {
-        title: "Létezz könnyedén",
-        description: "Az egész oldalt én csináltam Angularral a megrendelő ötlete szerint készült a design.",
-        image: "./assets/létezz_könnyedén_logo.png",
-        logos: ["assets/angular_logo.png"]
-    },
-    
-    {
-        title: "BME Környezetgazdaság tanszék",
-        description: "A backendet is én csináltam (Laravel ) és a frontendet a bladek jelenítik meg (SSR).",
-        image: "./assets/bme_logo.png",
-        logos: ["assets/laravel_logo.png","assets/mysql_logo.png"]
-    },
-    {
-        title: "Zöldséges",
-        description: "A frontendet csináltam ebben a projektben, együtt működve az UI csapattal és backendes csapattal",
-        image: "./assets/zoldseges_logo.png",
-        logos: ["assets/angular_logo.png"]
-    },
-    {
-        title: "Futárom",
-        description: "A frontendet csináltam ebben a projektben, együtt működve az UI csapattal és backendes csapattal",
-        image: "./assets/futarom_logo.png",
-        logos: ["assets/JS_logo.png"]
-    },
-    {
-        title: "Pimpa",
-        description: "A backendet is én csináltam ( Laravel ) és a frontendet a bladek jelenítik meg (SSR).",
-        image: "./assets/pimpa_logo.png",
-        logos: ["assets/laravel_logo.png","assets/mysql_logo.png"]
-    },
-    {
-        title: "Ammarna",
-        description: "A frontendet ( Angular ) csináltam ebben a projektben, együtt működve az UI csapattal és backendes csapattal",
-        image: "./assets/ammarna_logo.png",
-        logos: ["assets/angular_logo.png"]
-    },
+import {data } from './data.js';
 
-]
 document.addEventListener('DOMContentLoaded', function () {
     writeSkillList();
     writeWorkList();
@@ -60,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function writeSkillList() {
     const skilldiv = document.getElementById('skills');
     let text = ``;
-    for (let skill of skills) {
+    for (let skill of data.skills) {
         text += `<div class="d-flex align-items-center">`;
         for (let box of Array.from({ length: skill.full })) {
             text += `<div class="skill-box full"></div>`;
@@ -78,7 +27,7 @@ function writeSkillList() {
 function writeWorkList() {
     const workdiv = document.getElementById('works');
     let text = ``;
-    for (let work of works) {
+    for (let work of data.works) {
         text += `<div class="work">`;
         text += `<h3>${work.title}</h3>`;
         text += `<p class="work-description">${work.description}</p>`;
